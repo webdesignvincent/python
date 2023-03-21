1d6b353370f91a45c6f79da0a01ad3f8a8a8857f
Holger Vicente <webdesignvincent@gmail.com>
2023-03-21 18:17:56 -0500

    primer archivo

---
+++ git.py
@@ -0,0 +1,47 @@
+git --version
+
+--configurar usuario
+git config --global user.name "Holger Vicente"
+
+--ver usuario 
+git config --global user.name
+
+--configurar correo
+git config --global user.email "webdesignvincent@gmail.com"
+
+--activar los colores
+git config --global color.ui true
+
+comandos
+iniciar repositorio
+git init
+
+ver cual el estado actual de tu repositorio
+git status
+
+fase antes de enviar nuestros archivos a una nueva version
+git add conversor_python_funcion_clause.py
+git add conversor_python_funcion.py
+git add conversor_python.py
+
+git status
+
+guardar nueva version
+git commit -m "Agregando conversor_python_funcion_clause"
+git commit -m "Agregando conversor_python_funcion"
+git commit -m "Agregando conversor_python"
+
+agregar
+git add .
+git status
+
+quitar de del styleindex
+git rm --cached index.html
+
+git commit -m "Agregando Practicas en Python"
+
+en el repositorio agrega un archivo .gitignore para excluir archivos
+dentro de eso escribir la extension del archivo que desea ingorar
+*.psd
+contacto.html
+
